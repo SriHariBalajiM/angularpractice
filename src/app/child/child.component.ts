b1eefdee0f34a807c6260b89c1a5bd8eb69e28a5
SriHariBalajiM <srihari41106@gmail.com>
2025-11-13 00:10:35 +0530

    input and output signal based data communication

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { ChangeDetectionStrategy, Component, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, DoCheck, EventEmitter, input, Input, OnChanges, OnInit, output, Output, signal, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,25 +8,28 @@ import { ChangeDetectionStrategy, Component, DoCheck, EventEmitter, Input, OnCha
   styleUrl: './child.component.scss',
   encapsulation:ViewEncapsulation.None,
 })
-export class ChildComponent implements OnChanges,OnInit {
+export class ChildComponent {
 
   @Input()
   str:string[]=[];
 
-  @Input() keyValue='';
-  @Output() sendMsg= new EventEmitter();
+  keyValue=input<string>();
+  sendMsg= output<string>();
 
   randomObj:{prop:string,val:string}[]=[{prop:'color',val:'black'},{prop:'color',val:'white'},{prop:'animal',val:'cat'},{prop:'animal',val:'dog'}];
-  filteredObj:{prop:string,val:string}[]=[];
+  filteredObj=computed(()=>{
+    return this.keyValue() ? this.randomObj.filter(r => r.prop.toLowerCase() === this.keyValue()?.toLowerCase()) : [...this.randomObj];
+  });
   
-  ngOnInit(): void {
-    this.filteredObj=[...this.randomObj];
-  }
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['keyValue']) {
-    this.filteredObj=this.randomObj.filter(a=>a.prop.toLowerCase()===this.keyValue.toLowerCase());
-    }
-  }
+  // ngOnInit(): void {
+  //   this.filteredObj=[...this.randomObj];
+  // }
+  // ngOnChanges(changes: SimpleChanges) {
+  //   if (changes['keyValue']) {
+  //   this.filteredObj=this.randomObj.filter(a=>a.prop.toLowerCase()===this.keyValue.toLowerCase());
+  //   }
+  // }
+
 
 click(){
 this.str.push('Child component');
